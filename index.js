650d56ca1506eae9d65dfd032cf8075f39992cf5
Huong <huongnguyenak96@gmail.com>
2019-06-24 20:39:23 +0700

    dest

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const app = require('express')();
 app.set('view engine', 'ejs');
+const multer = require('multer');
+const upload = multer({ dest: 'public/images/' })
+
+const single = upload.single('avatar')
 
 app.get('/upload', (req,res)=>{
     res.render('upload');
 })
-app.post('/upload', (req,res)=>{
-    
+app.post('/upload', single, (req,res)=>{
+    const avatar = req.file;
+    const name = req.body.txtName
+    res.send({ avatar, name })
 })
 
 app.listen(3000,()=>console.log('Server start on port 3000'));
